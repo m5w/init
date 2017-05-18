00357f3ccc34204228d79cfdf6b09b527b577163
Matthew <m5w@users.noreply.github.com>
2017-05-18 17:22:55 -0400

    Install vim

--- init.sh
+++ init.sh
@@ -39,10 +39,12 @@ exit 1
 #mkdir -p github.com/m5w
 #cd github.com/m5w
 # <https://superuser.com/a/484330>
-SUDO_HOME="$(getent passwd $SUDO_USER|cut -d: -f6)"
-cd "$SUDO_HOME/github.com/m5w"  # github.com/m5w/init/ should already exist.
+su "$SUDO_USER" <<\LF
+cd "~/github.com/m5w"  # github.com/m5w/init/ should already exist.
 git clone https://github.com/m5w/logger
-cd logger
+LF
+SUDO_HOME="$(getent passwd $SUDO_USER|cut -d: -f6)"
+cd "$SUDO_HOME/github.com/m5w/logger"
 install -Dt /opt/logger/bin logger.sh
 
 # Add /opt/logger/bin to the PATH so that we can use logger. /etc/profile will
@@ -81,3 +83,27 @@ cd stow
 git checkout VirtualBox
 stow grub
 update-grub
+
+# Configure APT. We need source code to run
+#         logger.sh apt-get -qy build-dep vim-gtk
+
+cd /etc/apt
+git clone https://github.com/m5w/etc-apt-stow.git stow
+trash-put sources.list
+cd stow
+stow apt
+logger.sh apt-get -qy update
+
+# Install vim.
+
+logger.sh apt-get -qy build-dep vim-gtk
+su "$SUDO_USER" <<\LF
+cd "~/github.com/m5w"
+git clone https://github.com/m5w/vim.git
+cd vim
+git checkout nuw
+./configure --prefix=/opt/vim --with-features=huge --enable-luainterp=yes --enable-perlinterp=yes --enable-python3interp=yes --enable-tclinterp=yes --enable-rubyinterp=yes --enable-gui=gtk2 --with-python3-config-dir=/usr/lib/python3.5/config-3.5m-x86_64-linux-gnu
+make
+LF
+cd "$SUDO_HOME/github.com/m5w/vim"
+make install
