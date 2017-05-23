d7a2a9f69b9df55f7ab3283e0376ceef881cc884
matthew <m5w@users.noreply.github.com>
2017-05-22 22:01:58 -0400

    Explain preconditions

--- init.sh
+++ init.sh
@@ -16,35 +16,51 @@
 set -eo pipefail
 unalias -a
 
-# This script needs to run as root. However, it also needs to run:
+# This script needs to run as the superuser. However, it also needs to run as
+# the user.  sudo solves this problem by setting the environmental variable
+# SUDO_USER to ``the login name of the user who invoked sudo"[^1], and so, if
+# this script is executed (as the superuser) with sudo, it could run
 #
-#      -  as the user and
+#     su "$SUDO_USER" << LF
+#     echo "Hello, world!"
+#     LF
 #
-#      -  under the user's home directory, but as root.
+# to execute ``echo "Hello, world!"`` as the user. (This script could also be
+# executed as a user other than the superuser with sudo with the
+# ``-u user, --user=user`` option of sudo, but then, if the script were to run
+# the above code, su would (interactively) prompt for a password. All commands
+# in this script must be non-interactive.)
 #
-# Therefore, the user must use sudo to run this script as root so that this
-# script can use
-#
-#         sudo -iu "$SUDO_USER" bash << LF
-#
-# to run as the user and
-#
-#         "$(getent passwd "$SUDO_USER"|cut -d: -f6)"
-#
-# to get the user's home directory.
+# terminal-logger uses HOME to determine where to look for a .terminal-logger
+# directory (at ``"$HOME/.terminal-logger"``), and trash-put appears to do the
+# same to determine where to look for the Trash. If terminal-logger does not
+# find a .terminal-logger directory where it first looks, it makes one. This
+# new .terminal-logger directory has the same owner as the user as which
+# terminal-logger runs. If the user runs this script as the superuser, but not
+# on a login shell with sudo, HOME is still the user's home directory, not that
+# of the superuser. Therefore, when this script would first run
+# terminal-logger, terminal-logger would look in the user's home directory for
+# a .terminal-logger directory, not find one, and make a new .terminal-logger
+# directory under the user's home directory but owned by the superuser. When
+# the user would later run terminal-logger, terminal-logger would find this
+# newly-made directory and try to open the lock file, which is under the
+# directory. However, since the directory is owned by the superuser, and
+# terminal-logger would be running as the user, terminal-logger would not have
+# permission to open the lock file. A similar problem would occur with
+# trash-put, which appears to make Trash at ``"$HOME/.local/share/Trash"`` if
+# Trash does not already exist and tries to open directories under Trash.
+# Therefore, the user must also execute this script on a login shell. The
+# script actually runs as the user by running ``sudo -iu "$SUDO_USER" << LF``
+# instead of ``su "$SUDO_USER" << LF``, since the later would execute with HOME
+# as the superuser's home directory.
 
-[[ "$EUID" -eq 0 ]] && [[ -n "$SUDO_USER" ]] || {
-echo "$0: You must use sudo to execute this program as the superuser"
-exit 1
-}
-
-function get_home {
-        # cf. <https://superuser.com/a/484330>.
+_get_home () {
+        # cf. <https://superuser.com/a/484330>
         getent passwd "$1"|cut -d: -f6
 }
 
-[[ "$HOME" == "$(get_home 0)" ]] || {
-echo "$0: You must run this script from a login shell"
+(($EUID == 0)) && [[ $HOME == $(_get_home 0) ]] && [[ -n $SUDO_USER ]] || {
+echo "$0: You must invoke sudo to execute this program as the superuser on a login shell"|fold --spaces --width=79
 exit 1
 }
 
@@ -67,7 +83,7 @@ cd github.com/m5w
 
 git clone https://github.com/m5w/terminal-logger.git terminal-logger
 LF
-SUDO_HOME="$(get_home "$SUDO_USER")"
+SUDO_HOME="$(_get_home "$SUDO_USER")"
 cd "$SUDO_HOME/github.com/m5w/terminal-logger"
 install -Dt /usr/local/bin terminal-logger
 
